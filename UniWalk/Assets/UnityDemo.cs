c496da125853d9af9328012fae355eaab829792e
KMN114514 <teraumareninja@gmail.com>
2016-11-21 01:21:49 +0900

    歩くの

---
+++ UniWalk/Assets/UnityDemo.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class UnityDemo : MonoBehaviour {
+
+	int Jf = 0;
+
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w")) {
+			transform.position += transform.forward * 0.1f;
+			animator.SetBool("isRun", true);
+		} else {
+			animator.SetBool("isRun", false);
+		}
+		if (Input.GetKey("d")) {
+			transform.Rotate(0, 5, 0);
+		}
+		if (Input.GetKey ("a")) {
+			transform.Rotate(0, -5, 0);
+		}
+
+		if (Input.GetKey ("space") && Jf == 0) {
+		Rigidbody Uni = GetComponent<Rigidbody>();
+			Uni.AddForce(0, 500, 0);
+			animator.SetBool("Jamp", true);
+			Jf = 1;
+		}
+	}
+
+	void OnCollisionEnter () {
+		animator.SetBool("Jamp", false);
+		Jf = 0;
+	}
+
+
+}
---
+++ UniWalk/Assets/Up_Down.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Up_Down : MonoBehaviour {
+	float move = 0.01f;
+	int mf = 0;
+	void Update () {
+		this.transform.position += new Vector3 (0,move,0);
+
+		if (move > 1.0f) {
+			mf = 1;
+		}
+		if (move < 0.0f) {
+			mf = 0;
+		}
+
+		if (mf == 0) {
+			move += 0.01f;
+		} else {
+			move -= 0.01f; 
+		}
+
+
+
+	}
+}
