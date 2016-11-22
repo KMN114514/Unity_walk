3950dd71c872c1e747fcc3991b73bd9a0b397108
KMN_note <teraumareninja@gmail.com>
2016-11-22 20:35:10 +0900

    Tank

---
+++ UniWalk/Assets/Bomb.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var ExplosionPrefab:GameObject;
+
+
+
+function OnCollisionEnter (WHO : Collision) {
+
+	Instantiate(
+
+		ExplosionPrefab,
+
+		transform.position,
+
+		transform.rotation
+
+	);
+
+	Destroy(gameObject);
+
+}
+
